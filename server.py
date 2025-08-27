ea2d24c64dfdbeb61b74cd5225a4ea71510dc13b
__Templar__/// <145387566+Big-Daddy-Maxim@users.noreply.github.com>
2025-08-27 23:04:46 +0300

    Update server.py amo keys

--- server.py
+++ server.py
@@ -27,9 +27,9 @@ def favicon():
     return '', 204
 
 def get_scope_id():
-    secret = 'ВАШ_SECRET_KEY'  # Из настроек канала в AmoCRM
-    channel_id = 'ВАШ_CHANNEL_ID'  # ID вашего канала в AmoCRM
-    amojo_id = 'ВАШ_AMOJO_ID'  # Получите из AmoCRM (API /account?with=amojo_id)
+    secret = 'Y3VUaXN7Shga5rbXO6lZRGhvKcBG4BhTQinEERh932BbbPOSZ8pHBAhI0qn2b2uG'  # Секретный ключ AmoCRM
+    channel_id = '1f79e336-e5e9-4968-adb6-b6a103f54145'  # ID интеграции в AmoCRM
+    amojo_id = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjM1Y2E2NTUxOTViY2Q5MDUyNmYxNDk5OTRjZWYzMzdmZDQxODUxNDkzMmYyZDQwMWJmNWMyZTYxYzlmNzRhYzI0MmZiMmQzOWQzMzc5MjJkIn0.eyJhdWQiOiIxZjc5ZTMzNi1lNWU5LTQ5NjgtYWRiNi1iNmExMDNmNTQxNDUiLCJqdGkiOiIzNWNhNjU1MTk1YmNkOTA1MjZmMTQ5OTk0Y2VmMzM3ZmQ0MTg1MTQ5MzJmMmQ0MDFiZjVjMmU2MWM5Zjc0YWMyNDJmYjJkMzlkMzM3OTIyZCIsImlhdCI6MTc1NjMyNDkyOSwibmJmIjoxNzU2MzI0OTI5LCJleHAiOjE5MTQwMTkyMDAsInN1YiI6IjEyNTYxODk4IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMyNDU0OTkwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwidXNlcl9mbGFncyI6MCwiaGFzaF91dWlkIjoiMDA3YTkxMGQtYzQ5Zi00MjFkLWE4YTUtYWY2M2Q4ZmY3YmQzIiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.atkN4IrSMuw_HocIQPicl_DQ_td-4gEMKFbRF_Su_Lg8Cdw6J3pdV-ZRS-QTB3SV7eVxJbqQHV9Hhi3zAWU3k8kO3BVtxabPDgAFKuhnKXw7MxicgHLPeJtandIwp5mzrMg9-s4ULP-KIwioPexNIVXzmlB6JE8e6fNT5iw7xEqP8M3LT8vdzic0YrvBeAZ_8Suu7wvdsMwhnApX26Safl9Dmx59MCodpG58cQK1G_O_bFIeLRl6PMc3_Ay08HwgiPJhZn5cxzd3L0V_En_5fTYqPftQ6xADBv4b-nIw831dyGAz6fzZY8HZ5XiIQVVC_GCW6MiiEmcepjp7SmuEyQ'  # Долгосрочный AmoCRM (API /account?with=amojo_id)
     path = f'/v2/origin/custom/{channel_id}/connect'
     url = f'https://amojo.amocrm.ru{path}'
 
